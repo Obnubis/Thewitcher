a3ee38d11dcf68393b21c03a0e2f1e6540da5502
Mario <Marioroblespad@gmail.com>
2023-01-11 13:40:01 +0100

    método isDeath

--- src/thewitcher/witcher.java
+++ src/thewitcher/witcher.java
@@ -35,11 +35,31 @@ public class witcher {
 		this.hasSoul = hasSoul;
 	}
 
-	
+	public boolean isDeath() {
+		if(health==0){
+			return true;
+		}
+		else if(!hasSoul) {
+			return false;
+		}
+		else {
+			return false;
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		witcher brujo = new witcher();
+		brujo.setName("Geralt de Rivia");
+		brujo.setHealth(15);
+		brujo.setHasSoul(true);
+		if(brujo.isDeath()) {
+			System.out.println(brujo.getName()+ " está muerto");
+		}
+		else {
+			System.out.println(brujo.getName()+" está vivo");
+		}
 	}
 
 }
