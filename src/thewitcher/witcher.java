207864ee51864d281c118a525364f20a75725171
Mario <Marioroblespad@gmail.com>
2023-01-11 12:39:41 +0100

    añadidos atributos

--- src/thewitcher/witcher.java
+++ src/thewitcher/witcher.java
@@ -1,6 +1,10 @@
 package thewitcher;
 
 public class witcher {
+	private String name;
+	private int health;
+	private boolean hasSoul;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
