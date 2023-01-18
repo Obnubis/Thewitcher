883950722eb75094d4ce62d4f6aa7bcf00080292
Mario <Marioroblespad@gmail.com>
2023-01-18 12:36:20 +0100

    creada la clase principal llamada Main

---
+++ src/thewitcher/Main.java
@@ -0,0 +1,21 @@
+package thewitcher;
+
+public class Main {
+	
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		witcher brujo = new witcher();
+		brujo.setName("Geralt de Rivia");
+		brujo.setHealth(15);
+		brujo.setHasSoul(true);
+		if(brujo.isDeath()) {
+			System.out.println(brujo.getName()+ " está muerto");
+		}
+		else {
+			System.out.println(brujo.getName()+" está vivo");
+		}
+	}
+	
+}
--- src/thewitcher/witcher.java
+++ src/thewitcher/witcher.java
@@ -48,18 +48,4 @@ public class witcher {
 		
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		witcher brujo = new witcher();
-		brujo.setName("Geralt de Rivia");
-		brujo.setHealth(15);
-		brujo.setHasSoul(true);
-		if(brujo.isDeath()) {
-			System.out.println(brujo.getName()+ " está muerto");
-		}
-		else {
-			System.out.println(brujo.getName()+" está vivo");
-		}
-	}
-
 }
